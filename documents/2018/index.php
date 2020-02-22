b2a9b69201527886e1e576512abc94ad310a95bf
philwlowder <lowder.phil@gmail.com>
2020-02-22 10:38:21 -0500

    adding documents
    bulletins

---
+++ documents/2018/index.php
@@ -0,0 +1,237 @@
+
+
+<?php ob_start();?>
+
+<?php
+    $pagetitle = "Bulletin | Stony Hill UMC";
+    $description = "the weekly church bulletin";
+?>
+
+<?php include ("head.html"); ?>
+
+</head>
+
+<style>
+
+
+.flex_unordered {
+    display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+}
+
+.flex_list {
+margin: 25px;
+list-style-position: inside;
+list-style-image: circle;
+}
+
+</style>
+
+<body>
+
+    <?php include ("nav.html"); ?>
+
+    <div class="content">
+
+        <h1>2018 Bulletin</h1>
+
+
+        <ul class="flex_unordered large_font">
+
+        <li class="flex_list">
+            <a href="/documents/2018/december-30-2018.docx">December 30th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/december-23-2018.docx">December 23rd</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/december-16-2018.docx">December 16th</a>
+        </li>
+
+
+          <li class="flex_list">
+            <a href="/documents/2018/december-02-2018.docx">December 2nd</a>
+        </li>
+
+
+         <li class="flex_list">
+            <a href="/documents/2018/november-25-2018.docx">November 25th</a>
+        </li>
+
+
+         <li class="flex_list">
+            <a href="/documents/2018/november-18-2018.docx">November 18th</a>
+        </li>
+
+         <li class="flex_list">
+            <a href="/documents/2018/november-11-2018.docx">November 11th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/november-04-2018.docx">November 4th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/october-28-2018.docx">October 28th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/october-21-2018.docx">October 21st</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/october-14-2018.docx">October 14th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/october-07-2018.docx">October 7th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/september-30-2018.docx">September 30th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/september-23-2018.docx">September 23rd</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/september-16-2018.docx">September 16th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/september-09-2018.docx">September 9th</a>
+        </li>
+
+        <li class="flex_list">
+            <a href="/documents/2018/september-02-2018.docx">September 2nd</a>
+        </li>
+
+                <li class="flex_list">
+                        <a href="/documents/2018/august-26-2018.docx">August 26th</a>
+                    </li>
+
+                <li class="flex_list">
+                        <a href="/documents/2018/august-19-2018.docx">August 19th</a>
+                    </li>
+
+                <li class="flex_list">
+                        <a href="/documents/2018/august-12-2018.docx">August 12th</a>
+                    </li>
+
+                <li class="flex_list">
+                        <a href="/documents/2018/august-05-2018.docx">August 5th</a>
+                    </li>
+
+
+                <li class="flex_list">
+                        <a href="/documents/2018/july-29-2018.docx">July 29th</a>
+                    </li>
+
+            <li class="flex_list">
+                <a href="/documents/2018/july-22-2018.docx">July 22nd</a>
+            </li>
+
+            <li class="flex_list">
+                <a href="/documents/2018/july-15-2018.docx">July 15th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/july-08-2018.docx">July 8th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/july-01-2018.docx">July 1st</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/june-24-2018.docx">June 24th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/june-17-2018.docx">June 17th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/june-10-2018.docx">June 10th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/june-03-2018.docx">June 3rd</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/may-27-2018.docx">May 27th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/may-20-2018.docx">May 20th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/may-13-2018.docx">May 13th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/may-06-2018.docx">May 6th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/april-29-2018.docx">April 29th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/april-22-2018.docx">April 22nd</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/april-15-2018.docx">April 15th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/april-08-2018.docx">April 8th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/april-01-2018.docx">April 1st</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/march-25-2018.docx">March 25th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/march-18-2018.docx">March 18th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/march-11-2018.docx">March 11th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/march-04-2018.docx">March 4th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/february-25-2018.docx">February 25th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/february-18-2018.docx">February 18th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/february-11-2018.docx">February 11th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/february-04-2018.docx">February 4th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/january-28-2018.docx">January 28th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/january-21-2018.docx">January 21st</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/january-14-2018.docx">January 14th</a>
+            </li>
+            <li class="flex_list">
+                <a href="/documents/2018/january-07-2018.docx">January 7th</a>
+            </li>
+
+        </ul>
+
+    </div>
+    <!-- end  container -->
+
+    <?php include("footer.html"); ?>
+    <?php include("scripts.html"); ?>
+
+</body>
+
+</html>
+
+<?php file_put_contents("index.html", ob_get_contents())?>;ob_flush();
+
